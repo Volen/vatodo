22b37dd8d97f4f4e45d403158dd3f10e99ed54b2
Vasiliy Voyakin <vaulttx@yandex.ru>
2020-07-22 17:19:11 +0300

    add taskName

--- src/App.js
+++ src/App.js
@@ -2,41 +2,31 @@ import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [tasks, setTasks] = React.useState([
-    {
-      id: '0',
-      task: "buy milk",
-      completed: false,
-    },
-    {
-      id: '1',
-      task: "buy bread",
-      completed: false,
-    },
-    {
-      id: '2',
-      task: "buy new gaming pc",
-      completed: true,
-    }
-  ]);
+  const [tasks, setTasks] = React.useState([]);
+  const [taskName, setTaskName] = React.useState('');
 
   const listTasks = tasks.map(({ id, task, completed }) => <li key={id} className={`completed-${completed.toString()}`}>{task}</li>);
+
+  const handleChange = event => {
+    setTaskName(event.target.value);
+  }
 
   const submitHandler = () => {
     const newTask = {
       id: uuidv4(),
-      task: document.getElementById('task-input').value,
+      task: taskName,
       completed: false
     };
     console.log(newTask);
     setTasks(tasks.concat(newTask));
+    setTaskName('');
   }
 
   return (
     <div>
       <h1>TODO list</h1>
 
-      <input id="task-input" type="text" placeholder="add new task here" />
+      <input type="text" placeholder="add new task here" value={taskName} onChange={handleChange} />
       <button onClick={submitHandler}>Add task</button>
 
       <ul>

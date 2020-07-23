5ddfabc5aaa7dbca768684ce06cb1606e5555565
Vasiliy Voyakin <vaulttx@yandex.ru>
2020-07-23 14:16:32 +0300

    add completedHandler

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./task.css";
 
-const TaskList = ({ tasksList, deleteHandler }) => (
+const TaskList = ({ tasksList, deleteHandler, completeHandler }) => (
   <ul>
     {tasksList.map(({ id, task, completed }) => (
       <li key={id} className={`completed-${completed.toString()}`}>
-        [{task}]<button>Complete task</button>
-        <button data-id={id} onClick={deleteHandler}>Delete Task</button>
+        [{task}]
+        <button data-id={id} onClick={completeHandler}>
+          Complete task
+        </button>
+        <button data-id={id} onClick={deleteHandler}>
+          Delete Task
+        </button>
       </li>
     ))}
   </ul>
@@ -41,16 +46,35 @@ function App() {
       task: taskName,
       completed: false,
     };
-    setTasks(tasks.concat(newTask));
     localStorage.setItem("tasks", JSON.stringify(tasks.concat(newTask)));
+    setTasks(tasks.concat(newTask));
     setTaskName("");
   };
 
   const deleteHandler = (event) => {
-    const id = event.target.getAttribute('data-id');
+    const id = event.target.getAttribute("data-id");
     const newList = tasks.filter((item) => item.id !== id);
-    setTasks(newList);
     localStorage.setItem("tasks", JSON.stringify(newList));
+    setTasks(newList);
+  };
+
+  const completeHandler = (event) => {
+    const id = event.target.getAttribute("data-id");
+    const newList = tasks.map((task) => {
+      if (task.id === id) {
+        const newTask = {
+          ...task,
+          completed: true,
+        };
+        return newTask;
+      } else {
+        return task;
+      }
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(newList));
+    console.log(newList);
+    setTasks(newList);
   };
 
   return (
@@ -63,7 +87,11 @@ function App() {
         submitHandler={submitHandler}
       />
 
-      <TaskList tasksList={tasks} deleteHandler={deleteHandler}/>
+      <TaskList
+        tasksList={tasks}
+        deleteHandler={deleteHandler}
+        completeHandler={completeHandler}
+      />
     </div>
   );
 }
